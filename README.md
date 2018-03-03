7f23fd53b10989d1bd2a4f549028a3ed27b74d40
Nick Whyte <nick@nickwhyte.com>
2018-03-03 11:14:39 +1100

    Continuous Deployment

---
+++ README.md
@@ -0,0 +1,27 @@
+# Github Info
+
+Show GitHub information on Git pre-push or on demand
+
+### Requirements
+
+- Python >= 3.6
+
+### Setup
+
+```
+pip3 install github-info
+```
+
+### Usage
+
+#### 1. Configure the tool
+
+```
+github-info configure
+```
+
+#### 2. Use it
+
+```
+github-info
+```
--- setup.py
+++ setup.py
@@ -1,9 +1,33 @@
 from setuptools import setup
+import os
+
+def get_version():
+    version_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        'VERSION')
+    v = open(version_path).read()
+    if type(v) == str:
+        return v.strip()
+    return v.decode('UTF-8').strip()
+
+readme_path = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)),
+    'README.md',
+)
+long_description = open(readme_path).read()
+
+try:
+    version = get_version()
+except Exception as e:
+    version = '0.0.0-dev'
+
 
 setup(
     name='github-info',
-    version='0.0.1',
+    version=version,
     description='Show GitHub information on Git pre-push or on demand',
+    long_description=long_description,
+    zip_safe=False,
     url='http://github.com/nickw444/github-info',
     author='Nick Whyte',
     author_email='nick@nickwhyte.com',
